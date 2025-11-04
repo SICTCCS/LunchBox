671b3a489ef3c9a07a8ccc881003c87baec48520
ImperialOfficer <payne.kenneth.54321@gmail.com>
2025-11-04 12:33:33 -0600

    fixed images and made menus dynamic

--- footer.js
+++ footer.js
@@ -4,6 +4,6 @@ footer.innerHTML = `
 Maintained & Built by SICTC Computer Science Software Development <br>
 The LunchBox <br>
 1901 Lynch Rd, Evansville, IN 47711 <br>
-<a href="https://sictc.evscschools.com/"><img src="content/footerSICTC.png" style="width:90%;height:auto;" alt="footerSICTC"></a>`;
+<a href="https://sictc.evscschools.com/"><img src="content/footerSICTC.png" alt="footerSICTC"></a>`;
 
 document.querySelector("body").appendChild(footer);
